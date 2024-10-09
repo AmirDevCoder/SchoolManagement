0fa50deba354fb325d606782a60e3fc954823cc9
emamagic <emamagic.dev@gmail.com>
2024-10-09 21:38:58 +0330

    Implement Database Config for Postgres and MongoDB

---
+++ src/data/db/dbconf/Mongo.java
@@ -0,0 +1,43 @@
+package data.db.dbconf;
+
+import lib.db.DBConfigAbstraction;
+import lib.db.DBConfiguration;
+import lib.db.DBType;
+
+// convention over configuration
+@DBConfiguration
+public class Mongo extends DBConfigAbstraction {
+
+    /// we can inject configuration file here
+
+    @Override
+    public DBType getDBType() {
+        return DBType.MONGODB;
+    }
+
+    @Override
+    public String getDBName() {
+        return "school";
+    }
+
+    @Override
+    public String getHost() {
+        return "localhost";
+    }
+
+    @Override
+    public int getPort() {
+        return 27017;
+    }
+
+    @Override
+    public String getPassword() {
+        return "1377";
+    }
+
+    @Override
+    public String getUsername() {
+        return "emamagic";
+    }
+
+}
